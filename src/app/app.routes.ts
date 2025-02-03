bdce593933efe16819b636b8d7da3615e14b7056
Xang Hang <xanghang@gmail.com>
2025-02-02 23:42:11 -0600

    complete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PhonesComponent } from './components/phones/phones.component';
+import { PhoneDetailsComponent } from './components/phone-details/phone-details.component';
+import { PhoneEditComponent } from './components/phone-edit/phone-edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'phones', component: PhonesComponent },
+  { path: 'phones/:phoneId', component: PhoneDetailsComponent },
+  { path: 'edit/:phoneId', component: PhoneEditComponent },
+];
